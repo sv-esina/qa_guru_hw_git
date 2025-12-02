a0106de8e0f5be200de38fb7380f4a0fe87967e3
Esina Svetlana <montesum1@gmail.com>
2025-12-02 13:59:15 +0300

    added soutln Let me introduce my home work

--- src/main/java/com/gmail/montesum/Main.java
+++ src/main/java/com/gmail/montesum/Main.java
@@ -7,7 +7,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome to QA.GURU!");
-
+        System.out.println();
+        System.out.println("Let me introduce my home work");
 
         }
     }

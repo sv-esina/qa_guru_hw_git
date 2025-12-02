0db5dc599e42a7275825684afecd013d2f35df7a
sv-esina <montesum1@gmail.com>
2025-12-02 16:33:34 +0300

    Update Main.java

--- src/main/java/com/gmail/montesum/Main.java
+++ src/main/java/com/gmail/montesum/Main.java
@@ -3,11 +3,10 @@ package com.gmail.montesum;
 
     
 public class Main {
-    private static String msg="Hello and welcome to QA.GURU!";
+    private static String msg="Hello and welcome to QA.GURU!!!";
 
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf(msg);
         System.out.println();
         System.out.println("Let me introduce my home work");

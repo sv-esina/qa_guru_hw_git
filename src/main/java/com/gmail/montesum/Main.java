8f07c1ade5dd12e5dc48d626624a00209acce274
Esina Svetlana <montesum1@gmail.com>
2025-12-02 14:11:17 +0300

    corrected defects

--- src/main/java/com/gmail/montesum/Main.java
+++ src/main/java/com/gmail/montesum/Main.java
@@ -1,8 +1,10 @@
 package com.gmail.montesum;
 
-private static String msg="Hello and welcome to HW QA.GURU!"
+
     
 public class Main {
+    private static String msg="Hello and welcome to HW QA.GURU!";
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
